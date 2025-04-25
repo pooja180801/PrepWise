8d73ea2bc3d8f8656eed71c3552eb7e3b7176821
poojak <139669968+pooja180801@users.noreply.github.com>
2025-04-25 09:09:07 +0530

    Set up the firebase

---
+++ firebase/client.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDFntJELlFw1ntZ-_Ffv86jp9f12bZsIKY",
+  authDomain: "prepwise-ad3da.firebaseapp.com",
+  projectId: "prepwise-ad3da",
+  storageBucket: "prepwise-ad3da.firebasestorage.app",
+  messagingSenderId: "339943186168",
+  appId: "1:339943186168:web:f97e6906858f638dec250d",
+  measurementId: "G-4LRW5787YN"
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
